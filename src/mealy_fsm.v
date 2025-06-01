57cac763fb932d6e772fdfd6900090240350c432
KDCastilloR <kcastillor13@miumg.edu.gt>
2025-06-01 12:08:03 -0600

    Update mealy_fsm.v

--- src/mealy_fsm.v
+++ src/mealy_fsm.v
@@ -1,26 +1,25 @@
 `timescale 1ns / 1ps
+
 module mealy_fsm (
-    input clk,
-    input reset,
-    input [3:0] total,
-    input vendA,
-    input vendB,
-    output reg [3:0] cambio
+    input logic clk, reset,
+    input logic [3:0] total,
+    input logic vendA, vendB,
+    output logic [3:0] cambio
 );
 
-    reg [3:0] saldo;
+    logic [3:0] saldo;
 
-    always @(posedge clk or posedge reset) begin
+    always_ff @(posedge clk or posedge reset) begin
         if (reset) begin
             saldo <= 0;
             cambio <= 0;
         end else begin
             if (vendA) begin
-                saldo <= total - 4'd2;
-                cambio <= total - 4'd2;
+                saldo <= total - 2;
+                cambio <= total - 2;
             end else if (vendB) begin
-                saldo <= total - 4'd3;
-                cambio <= total - 4'd3;
+                saldo <= total - 3;
+                cambio <= total - 3;
             end else begin
                 saldo <= total;
                 cambio <= 0;
